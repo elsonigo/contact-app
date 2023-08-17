268e5b33c7a8653a9570ff68922a080085aff509
Jonas <jonas_manser@hotmail.com>
2023-08-17 15:32:57 +0200

    passing data to templates

--- main.go
+++ main.go
@@ -12,9 +12,20 @@ func main() {
 		Views: engine,
 	})
 
+	type Person struct {
+		Name string
+		Age  int
+	}
+
+	jonas := &Person{
+		Name: "jonas",
+		Age:  36,
+	}
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("index", fiber.Map{
-			"Title": "Index!",
+			"Title":  "Index!",
+			"Person": jonas,
 		}, "layouts/main")
 	})
 
