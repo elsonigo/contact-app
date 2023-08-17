09b0fef341b761f4a8e90c9376a88b059a461fd0
Jonas <jonas_manser@hotmail.com>
2023-08-17 15:18:38 +0200

    +templating

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
 
 func main() {
-	app := fiber.New()
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.Render("index", fiber.Map{
+			"Title": "Index!",
+		}, "layouts/main")
 	})
 
 	app.Listen(":3000")
