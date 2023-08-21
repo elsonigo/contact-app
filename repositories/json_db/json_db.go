f02f88c00017b8f005f51f231919bddeb2e75768
Jonas <jonas_manser@hotmail.com>
2023-08-21 14:44:55 +0200

    -comment

--- repositories/json_db/json_db.go
+++ repositories/json_db/json_db.go
@@ -11,7 +11,6 @@ import (
 	"github.com/google/uuid"
 )
 
-// https://github.com/bigskysoftware/contact-app/blob/master/contacts_model.py#L92
 type JsonDatabase struct {
 	contacts []*domain.Contact
 }
